0db6fcb29049fe70e9400371af666ed8d900ea54
Aman Kumar <aman@aikenist.com>
2023-06-26 10:25:36 +0530

    added backend

--- frontend/src/pages/Packages.jsx
+++ frontend/src/pages/Packages.jsx
@@ -1,16 +1,60 @@
+// import React from "react";
+// import Header from "../components/Header";
+// import Card from "../components/Card";
+// import Footer from "../components/Footer";
+
+// const baseURL = "http://localhost:1337/api/packages";
+
+
+// const Packages = () => {
+
+//   useEffect(() => {
+//     axios.get(baseURL).then((response) => {
+//       setPost(response.data);
+//     });
+//   }, []);
+// console.log(response)
+//   if (!post) return (
+//     <div>
+//       Loading...
+//     </div>
+//   );
+//   return (
+//     <>
+//       <Header/>
+//       <Card />
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Packages;
+
+
+import axios from "axios";
 import React from "react";
-import Header from "../components/Header";
-import Card from "../components/Card";
-import Footer from "../components/Footer";
 
-const Packages = () => {
-  return (
-    <>
-      <Header/>
-      <Card />
-      <Footer />
-    </>
+const baseURL = "http://localhost:1337/api/packages";
+
+export default function Packages() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+      console.log(response.data)
+    });
+  }, []);
+  if (!post) return (
+    <div>
+      Loading...
+    </div>
   );
-};
 
-export default Packages;
+  return (
+    <div>
+      {/* <h1>{datas.place}</h1>
+      <p>{post.body}</p> */}
+    </div>
+  );
+}
